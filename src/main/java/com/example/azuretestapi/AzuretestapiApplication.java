44d0e3c803f7a1939c07b63c98e7c2686ff5a106
mevlut <mevlutkaymaz@gmail.com>
2022-11-20 16:23:32 -0600

    all

---
+++ src/main/java/com/example/azuretestapi/AzuretestapiApplication.java
@@ -0,0 +1,31 @@
+package com.example.azuretestapi;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.json.JSONObject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class AzuretestapiApplication {
+
+    @CrossOrigin
+    @GetMapping("/greeting")
+    public Map<String, String> greeting(){
+        HashMap<String, String> map = new HashMap<>();
+        map.put("greeting", "merhaba");
+        return map;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzuretestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/azuretestapi/AzuretestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azuretestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuretestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
